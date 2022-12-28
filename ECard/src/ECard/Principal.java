93e0d40cec6a7a839e5c3a28136590e7644863ba
Ivmarin <ivanmarinclase@gmail.com>
2022-12-29 00:49:33 +0100

    Añadidos inicial y principal

---
+++ ECard/src/ECard/Principal.java
@@ -0,0 +1,9 @@
+package ECard;
+
+public class Principal {
+	public static void main(String[] args) {
+		
+		inicial i = new inicial();
+		i.setVisible(true);
+	}
+}
---
+++ ECard/src/ECard/inicial.java
@@ -0,0 +1,54 @@
+package ECard;
+
+import java.awt.BorderLayout;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class inicial extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					inicial frame = new inicial();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public inicial() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		
+		JButton bt1 = new JButton("XD");
+		bt1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("funciona");
+			}
+		});
+		contentPane.add(bt1);
+	}
+
+}
